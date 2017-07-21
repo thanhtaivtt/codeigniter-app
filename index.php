509214f3dba7a75efd30e19f5d58396f4072c709
Vu Thanh Tai <thanhtai96nd@gmail.com>
2017-07-21 16:12:39 +0700

    change vendor

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ switch (ENVIRONMENT) {
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-$system_path = 'vendor/thanhtaivtt/codeigniter-blade/framework/system';
+$system_path = 'vendor/thanhtaivtt/ci-core/framework/system';
 
 /*
  *---------------------------------------------------------------
