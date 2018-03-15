fe30744655c6f9ab1e3c2d60b46c935e066875f3
Taivt <taivt@eway.vn>
2018-03-15 19:50:02 +0700

    chaneg system folder

--- public/index.php
+++ public/index.php
@@ -89,7 +89,7 @@ switch (ENVIRONMENT) {
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-$system_path = 'vendor/thanhtaivtt/ci-core/framework/system';
+$system_path = '../vendor/thanhtaivtt/ci-core/src/system/';
 
 /*
  *---------------------------------------------------------------
